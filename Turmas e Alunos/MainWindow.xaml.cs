e3559060c7fa61684dd80a18f979cc5df937bce5
20211011110019 <20211011110019@CNAT407137>
2022-12-23 15:57:46 -0300

    Adicionar arquivos de projeto.

---
+++ Turmas e Alunos/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Turmas_e_Alunos
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TurmaWindow w = new TurmaWindow();
+            w.ShowDialog();
+            //Só usando o "W.Show" cria diversos objetos sem parar
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            AlunoWindow w = new AlunoWindow();
+            w.ShowDialog();
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            MatriculaWindow w = new MatriculaWindow();
+            w.ShowDialog();
+        }
+
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            DiarioWindow w = new DiarioWindow(); ;
+            w.ShowDialog();
+        }
+    }
+}
